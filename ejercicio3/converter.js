04f7dc1c60d4300f692c53c2e522ec3b7770ff83
Paolo Billordo <paolobillordo@gmail.com>
2024-06-06 02:21:46 -0300

    ejercicio 3 completo

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -62,8 +62,7 @@ class CurrencyConverter {
         if (fromCurrency == toCurrency) {
             return parseFloat(amount)
         }
-        try {
-            console.log(fromCurrency, toCurrency)
+        try {            
             const response = await fetch(this.apiUrl + `/latest?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
             if (response.ok) {
                 const amount_converted = await response.json();                
@@ -73,7 +72,29 @@ class CurrencyConverter {
             console.error(`Ocurrio un error: ${error.message}`);
             return null;
         }
+    }
 
+    async yesterdayCurrency(amount, fromCurrency, toCurrency) {
+        if (fromCurrency == toCurrency) {
+            return parseFloat(amount)
+        }
+        let today = new Date();
+        today.setDate(today.getDate() - 2);
+        let year = today.getFullYear();
+        let month = String(today.getMonth() + 1).padStart(2, '0');
+        let day = String(today.getDate()).padStart(2, '0');
+        let yesterday = `${year}-${month}-${day}`;
+        console.log(yesterday);
+        try {            
+            const response = await fetch(this.apiUrl + `/${yesterday}?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
+            if (response.ok) {
+                const amount_converted = await response.json();                
+                return amount_converted.rates[toCurrency.code]                
+            };
+        } catch (error) {
+            console.error(`Ocurrio un error: ${error.message}`);
+            return null;
+        }
     }
 }
 
@@ -106,10 +127,18 @@ document.addEventListener("DOMContentLoaded", async () => {
             toCurrency
         );
 
+        const yestardayAmount = await converter.yesterdayCurrency(
+            amount,
+            fromCurrency,
+            toCurrency
+        );
+
         if (convertedAmount !== null && !isNaN(convertedAmount)) {
             resultDiv.textContent = `${amount} ${
                 fromCurrency.code
-            } son ${convertedAmount.toFixed(2)} ${toCurrency.code}`;
+            } son ${convertedAmount.toFixed(2)} ${toCurrency.code}, al día de ayer eran 
+            ${yestardayAmount.toFixed(2)} ${toCurrency.code}, la diferencia es de 
+            ${(convertedAmount - yestardayAmount).toFixed(2)} ${toCurrency.code}`;
         } else {
             resultDiv.textContent = "Error al realizar la conversión.";
         }
